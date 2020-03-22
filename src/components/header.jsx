054c5f042bb3c85b923232cfa3c99f691497da8f
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-03-22 01:07:30 -0500

    added logo img and imported to index.js

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import origamiBird from '../assets/origami-bird.png';
 
 
 class Header extends React.Component {
@@ -6,7 +7,7 @@ class Header extends React.Component {
     return (
       <div className="jumbotron jumbotron-fluid header py-3">
         <div className="container text-center align-middle">
-        <img className="bird" src="/sethharbison/Downloads/origami-bird.png" alt="geometric bird design"/>
+        <img className="bird" src={origamiBird} alt="geometric bird design"/>
           <h1 className="display-4">Chirper</h1>
           <p className="lead">Tell them EVERYTHING</p>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header';
 import './App.css';
-import './assets/origami-bird.png';
+import origamiBird from './assets/origami-bird.png';
 
 ReactDOM.render(<Header />, document.getElementById('root'));
