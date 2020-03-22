5679955128658777c2865df8d12cdec2e8720b82
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-03-22 17:44:30 -0500

    added then deleted profile component; also added App.jsx to pool all components into

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import origamiBird from '../assets/origami-bird.png';
 
-
 class Header extends React.Component {
   render() {
     return (
-      <div className="jumbotron jumbotron-fluid header py-3">
-        <div className="container text-center align-middle">
-        <img className="bird" src={origamiBird} alt="geometric bird design"/>
-          <h1 className="display-4">Chirper</h1>
-          <p className="lead">Tell them EVERYTHING</p>
+      <main>
+        <div className="jumbotron jumbotron-fluid header py-3">
+          <div className="container text-center align-middle">
+            <img className="bird" src={origamiBird} alt="geometric bird design" />
+            <h1 className="display-4">Chirper</h1>
+            <p className="lead">Tell them EVERYTHING</p>
+          </div>
+          <br />
         </div>
-      </div>
+      </main>
+
+
+
     )
   }
 }
