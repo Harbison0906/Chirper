b4c8cc5ecdd8518fc299d2951ca33d9b26717fd8
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-03-28 22:48:00 -0500

    moved all components into the one App.jsx file and deleted the components files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Header from './components/Header.jsx';
-import Timeline from './components/Timeline.jsx';
-import NewChirp from './components/NewChirp.jsx';
+import origamiBird from './assets/origami-bird.png';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -13,36 +12,89 @@ class App extends React.Component {
     }
   }
 
-  handleInputChange = ( value ) =>{
+  handleInputChange = (value) => {
     this.setState({ user: value });
-}
+  }
 
   toggleLoggedIn() {
     this.setState({ loggedIn: true });
   }
 
-  loggedInPage() {
-    return (
-      <main>
-        <Header />
-        <NewChirp user={this.props.user} />
-        <hr />
-        <Timeline />
-      </main>
-    );
-  }
   render() {
     if (this.state.loggedIn) {
-      return <div>{this.loggedInPage()}</div>;
+      return (
+        <main>
+          <section className="header">
+            <div className="jumbotron jumbotron-fluid header py-3">
+              <div className="container text-center align-middle">
+                <img className="bird" src={origamiBird} alt="geometric bird design" />
+                <h1 className="display-4">Chirper</h1>
+                <p className="lead">Tell them EVERYTHING</p>
+              </div>
+              <br />
+            </div>
+          </section>
+          <section className="newChirp">
+            <div className="container">
+              <section className="row justify-content-center">
+                <article className="col-md-7">
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 className="card-title">{this.state.handleInputChange}</h5>
+                      <textarea className="form-control mb-3" aria-label="With textarea"></textarea>
+                      <a href="#" className="btn btn-primary">Chirp</a>
+                    </div>
+                  </div>
+                </article>
+              </section>
+            </div>
+          </section>
+          <hr />
+          <section className="timeline">
+            <main className="container">
+              <section className="row justify-content-center">
+                <div className="col-md-7">
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 className="card-title">User</h5>
+                      <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                      <a href="#" className="btn btn-primary">Edit Chirp</a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-7">
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                      <a href="#" className="btn btn-primary">Edit Chirp</a>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </main>
+          </section>
+
+        </main>
+      )
     } else {
       return (
         <article>
-          <Header />
+          <section className="header">
+            <div className="jumbotron jumbotron-fluid header py-3">
+              <div className="container text-center align-middle">
+                <img className="bird" src={origamiBird} alt="geometric bird design" />
+                <h1 className="display-4">Chirper</h1>
+                <p className="lead">Tell them EVERYTHING</p>
+              </div>
+              <br />
+            </div>
+          </section>
           <div className="input-group w-50 mx-auto">
             <input type="text" className="form-control" placeholder="Username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
             <div className="input-group-append">
-              <button className="btn logInButton" type="button" 
-              onClick={() => this.toggleLoggedIn()}>Log In</button>
+              <button className="btn logInButton" type="button"
+                onClick={() => this.toggleLoggedIn()}>Log In</button>
             </div>
           </div>
         </article>
@@ -51,4 +103,4 @@ class App extends React.Component {
   }
 }
 
-  export default App;
+export default App;
