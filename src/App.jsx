423597a52ab77358ca3a86a3b4b8536ddfa8d42e
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-03-29 14:54:48 -0500

    removed login page feature and worked more on styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,14 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      user: ' ',
-      loggedIn: false
+      user: 'Username',
     }
   }
 
-  handleInputChange = (value) => {
-    this.setState({ user: value });
-  }
-
-  toggleLoggedIn() {
-    this.setState({ loggedIn: true });
-  }
+  
 
   render() {
-    if (this.state.loggedIn) {
+    
       return (
         <main>
           <section className="header">
@@ -40,9 +33,9 @@ class App extends React.Component {
                 <article className="col-md-7">
                   <div className="card">
                     <div className="card-body">
-                      <h5 className="card-title">{this.state.handleInputChange}</h5>
-                      <textarea className="form-control mb-3" aria-label="With textarea"></textarea>
-                      <a href="#" className="btn btn-primary">Chirp</a>
+                      <h5 className="card-title">{this.state.user}</h5>
+                      <textarea className="shadow form-control mb-3" aria-label="With textarea"></textarea>
+                      <a href="#" className="btn">Chirp</a>
                     </div>
                   </div>
                 </article>
@@ -56,18 +49,18 @@ class App extends React.Component {
                 <div className="col-md-7">
                   <div className="card">
                     <div className="card-body">
-                      <h5 className="card-title">User</h5>
+                      <h5 className="card-title">{this.state.user}</h5>
                       <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                      <a href="#" className="btn btn-primary">Edit Chirp</a>
+                      <a href="#" className="btn">Edit Chirp</a>
                     </div>
                   </div>
                 </div>
                 <div className="col-md-7">
                   <div className="card">
                     <div className="card-body">
-                      <h5 className="card-title">Card title</h5>
+                      <h5 className="card-title">{this.state.user}</h5>
                       <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                      <a href="#" className="btn btn-primary">Edit Chirp</a>
+                      <a href="#" className="btn">Edit Chirp</a>
                     </div>
                   </div>
                 </div>
@@ -77,29 +70,7 @@ class App extends React.Component {
 
         </main>
       )
-    } else {
-      return (
-        <article>
-          <section className="header">
-            <div className="jumbotron jumbotron-fluid header py-3">
-              <div className="container text-center align-middle">
-                <img className="bird" src={origamiBird} alt="geometric bird design" />
-                <h1 className="display-4">Chirper</h1>
-                <p className="lead">Tell them EVERYTHING</p>
-              </div>
-              <br />
-            </div>
-          </section>
-          <div className="input-group w-50 mx-auto">
-            <input type="text" className="form-control" placeholder="Username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-            <div className="input-group-append">
-              <button className="btn logInButton" type="button"
-                onClick={() => this.toggleLoggedIn()}>Log In</button>
-            </div>
-          </div>
-        </article>
-      )
-    }
+    
   }
 }
 
